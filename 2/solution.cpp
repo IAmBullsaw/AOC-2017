35f24450523cedced6383796cb394118edaca202
Oskar Jansson <oskja067@student.liu.se>
2017-12-03 15:54:03 +0100

    adds: solution to 2.2

--- 2/solution.cpp
+++ 2/solution.cpp
@@ -14,11 +14,35 @@ int get_difference(string const& row) {
     });
   return abs(stoi(*result.first) - stoi(*result.second) );
 }
+
+int get_even_division(string const& row) {
+  stringstream iss{row};
+  vector<string> tokens{istream_iterator<string>{iss},
+      istream_iterator<string>{}};
+
+  for (unsigned i{0};i < tokens.size(); ++i) {
+    int a{stoi(tokens.at(i))};
+    for (unsigned j{i+1}; j < tokens.size(); ++j) {
+      int b{stoi(tokens.at(j))};
+      auto result = div(a,b);
+      if (result.rem == 0) {
+        return result.quot;
+      }
+      result = div(b,a);
+      if (result.rem == 0) {
+        return result.quot;
+      }
+    }
+  }
+
+  return 0;
+}
+
 int main() {
   string row{""};
   unsigned sum{0};
   while(getline(cin,row)) {
-    sum += get_difference(row);
+    sum += get_even_division(row);
   }
   cout << "Checksum is " << sum << endl;
   return 0;
