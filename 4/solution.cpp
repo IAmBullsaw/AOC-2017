bfb61cc92cc46c226c52efb5044314fccae1c681
Oskar Jansson <oskja067@student.liu.se>
2017-12-05 14:59:06 +0100

    adds: 4/test.txt and finishes 4.2

--- 4/solution.cpp
+++ 4/solution.cpp
@@ -3,8 +3,8 @@
 #include <vector>
 #include <sstream>
 #include <iterator>
+#include <algorithm>
 using namespace std;
-
 
 int puzzle1() {
   string line{""};
@@ -26,7 +26,33 @@ int puzzle1() {
   return result;
 }
 
+int puzzle2() {
+  string line{""};
+  int result{0};
+  while(getline(cin,line)) {
+    istringstream iss{line};
+    vector<string> tokens{istream_iterator<string>{iss},
+        istream_iterator<string>{}};
+    bool approved{true};
+    set<string> set;
+    cout << endl;
+    for (string token: tokens){
+      sort(token.begin(),token.end());
+      cout << token << endl;
+      if(auto [it, inserted] = set.insert(token); !inserted) {
+        approved = false;
+        break;
+      }
+    }
+    if (approved) {
+      cout << approved << endl;
+      ++result;
+    }
+  }
+  return result;
+}
+
 int main() {
-  cout << "Result is: " << puzzle1() << endl;
+  cout << "Result is: " << puzzle2() << endl;
   return 0;
 }
