a0ecb36194aab895a0d5a6ca7b0f2c21ca3d213e
Oskar Jansson <oskja067@student.liu.se>
2017-12-14 08:41:48 +0100

    adds: solution to 10.1

--- 10/solution.cpp
+++ 10/solution.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <algorithm>
 #include <vector>
+#include <iterator>
 
 using namespace std;
 
@@ -13,12 +14,13 @@ public:
     for (int i{0}; i < list_size; i++) list.at(i) = i;
   }
 
-  void tie(int const& length) {
+  void old_tie(int const& length) {
     cout << "Tieing " << length << " ..." << endl;
     reverse(list.begin(), list.begin() + length);
-    int skip = (length + skip_size) % list.size();
-    rotate(list.begin(), list.begin() + skip, list.end());
-    current_position = (current_position + length) % (list.size() - 1);
+
+    rotate(list.begin(), list.begin() + current_position, list.end());
+
+    current_position = (length + skip_size) % list.size();
     ++skip_size;
   }
 
@@ -32,15 +34,33 @@ public:
     }
     cout << endl;
     cout << "Current position: "<< current_position << endl;
-    cout << "Result: " << list.at(current_position)* list.at((current_position+1)%list.size())<< endl;
+    cout << "Result: " << list.at(0)*list.at(1) << endl;
   }
 
+  void tie(int const& length) {
+    if ( (current_position + length) < list.size()) { // don't go too far, it's only the first date
+      reverse(list.begin() + current_position, list.begin() + current_position + length);
+      current_position = (current_position + length + skip_size) % list.size(); // update ze position
+    }
+    else {
+      vector<int> hej;
+      int new_length = (current_position + length) % list.size(); // Get overflow
+      copy(list.begin() + current_position, list.end(), back_inserter(hej)); // copy from current to end
+      copy(list.begin(), list.begin() + new_length, back_inserter(hej)); // copy from beginning to length
+
+      reverse(hej.begin(),hej.end());
+      swap_ranges(list.begin() + current_position, list.end(), hej.begin()); // swap from beginning of hej
+      swap_ranges(list.begin(), list.begin() + new_length, hej.begin() + ( length - new_length ) ); // swap from beginning of hej+previous amount
+
+      current_position = (current_position + length + skip_size) % list.size(); // update ze position
+    }
+    ++skip_size;
+  }
 private:
   vector<int> list;
   int skip_size;
   int current_position;
 };
-
 
 void test() {
   Circle circle{5};
@@ -55,14 +75,16 @@ void test() {
   circle.print();
 }
 
-int main() {
-
+void solution1() {
   Circle circle{256};
   vector<int> input({183,0,31,146,254,240,223,150,2,206,161,1,255,232,199,88});
   for (int const& i: input) {
     circle.tie(i);
   }
   circle.print();
+}
 
+int main() {
+  solution1();
   return 0;
 }
